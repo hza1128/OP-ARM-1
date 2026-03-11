78df1ce721c7c4196b0a45ef813788e403963e94
hza1128 <141570215+hza1128@users.noreply.github.com>
2026-03-11 12:02:14 +0800

    Uncomment background image copy command
    Uncomment the line to copy the background image for the theme.

--- diy-part2.sh
+++ diy-part2.sh
@@ -75,7 +75,7 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 git clone -b v5-lua https://github.com/sbwml/luci-app-mosdns.git feeds/luci/applications/luci-app-mosdns
 
 # 修改主题背景
-# cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
